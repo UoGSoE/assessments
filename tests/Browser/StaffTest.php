b9ec9b8200125c02bd1f7de19130456e573c7b0a
Billy <william.allan@glasgow.ac.uk>
2017-03-17 09:10:24 +0000

    Added dusk browser test for staff updating the feedback completed field

--- tests/Browser/StaffTest.php
+++ tests/Browser/StaffTest.php
@@ -1,10 +1,34 @@
 <?php
+// @codingStandardsIgnoreFile
 
 namespace Tests\Browser;
 
 use Tests\DuskTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Carbon\Carbon;
 
 class StaffTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
+    /** @test */
+    public function staff_can_mark_assessment_feedback_as_having_been_given()
+    {
+        $this->browse(function ($browser) {
+            $now = Carbon::now();
+            $course = $this->createCourse();
+            $staff = $this->createStaff();
+            $course->staff()->sync([$staff->id]);
+            $assessment = $this->createAssessment(['course_id' => $course->id, 'user_id' => $staff->id, 'deadline' => Carbon::now()->subWeeks(4)]);
+            $browser->loginAs($staff)
+                    ->visit("/assessment/{$assessment->id}")
+                    ->click('#datepicker')
+                    ->click('[data-day="1"]')
+                    ->press('Save')
+                    ->assertSee('Feedback marked as complete')
+                    ->assertSee(Carbon::now()->startOfMonth()->format('d/m/Y'))
+                    ->assertDontSee('Save');
+        });
+        
+    }
 }
