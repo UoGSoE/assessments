822066768cef4d7d832b48e95e39e6e1f2aac62f
Billy <william.allan@glasgow.ac.uk>
2017-08-25 09:58:02 +0100

    WIP

--- app/Spreadsheet/SheetToDatabase.php
+++ app/Spreadsheet/SheetToDatabase.php
@@ -7,6 +7,47 @@ use App\Assessment;
 use App\User;
 use Carbon\Carbon;
 
+/**
+    0 => "Term"
+    1 => "Session"
+    2 => "Subject"
+    3 => "Catalog"
+    4 => "Career"
+    5 => "Descr"
+    6 => "course"
+    7 => "Class Stat"
+    8 => "Cap Enrl"
+    9 => "ID"
+    10 => "Last"
+    11 => "First Name"
+    12 => "email"
+    13 => "assignment 1 - title"
+    14 => "assignment 1 - method of submission"
+    15 => "assignment 1 - date of submission"
+    16 => "assignment 1 - date feedback due"
+    17 => "Graded feedback_1"
+    18 => "Method of feedback_1"
+    19 => "assignment 2 - title"
+    20 => "assignment 2 - method of submission"
+    21 => "assignment 2 - date of submission"
+    22 => "assignment 2 - date feedback due"
+    23 => "Graded feedback_2"
+    24 => "Method of feedback_2"
+    25 => "assignment 3 - title"
+    26 => "assignment 3 - method of submission"
+    27 => "assignment 3 - date of submission"
+    28 => "assignment 3 - date feedback due"
+    29 => "Graded feedback_3"
+    30 => "Method of feedback_3"
+    31 => "assignment 4 - title"
+    32 => "assignment 4 - method of submission"
+    33 => "assignment 4 - date of submission"
+    34 => "assignment 4 - date feedback due"
+    35 => "Graded"
+    36 => "feedback_4"
+    37 => "Method of feedback_4"
+    38 => "Additional comments"
+*/
 class SheetToDatabase
 {
     public $errors;
@@ -36,6 +77,19 @@ class SheetToDatabase
 
     public function rowToAssessment($row)
     {
+        $course = $row[6];
+        $staffEmail = strtolower($row[12]);
+        $ass1 = [
+            'title' => $row[13],
+            'submission_method' => $row[14],
+            'submission_date' => $row[15],
+            'feedback_method' => $row[18],
+            'is_graded' => $row[17],
+        ];
+        if (!$date) {
+            return false;
+        }
+        dd($ass1);
         $deadline = $row[0];
         if (!$deadline) {
             return false;
