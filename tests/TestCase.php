8d56ba52618e4b5fac47c75bd8367e8febcd8e2f
finlay.maciver <finlay.maciver@glasgow.ac.uk>
2018-09-21 13:55:30 +0100

    Reverted TestCase changes, as it didn't fix gitlab ci failing.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,22 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Exceptions\Handler;
 use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     //use DatabaseMigrations;
-    use RefreshDatabase;
+    use DatabaseSetup;
 
     protected function setUp()
     {
         parent::setUp();
-        // $this->setupDatabase();
+        $this->setupDatabase();
 
 //        $this->disableExceptionHandling();
     }
