951881cf9ba83bf0fd042baec7d33833af1f3f4d
Billy <william.allan@glasgow.ac.uk>
2021-01-29 14:06:57 +0000

    Switched from using box/spout directly to using ohffs/simple-spout

--- app/Spreadsheet/Spreadsheet.php
+++ app/Spreadsheet/Spreadsheet.php
@@ -5,34 +5,37 @@ namespace App\Spreadsheet;
 use Box\Spout\Common\Type;
 use Box\Spout\Reader\ReaderFactory;
 use Box\Spout\Writer\WriterFactory;
+use Ohffs\SimpleSpout\ExcelSheet;
 
 class Spreadsheet
 {
     public function import($filename)
     {
-        $reader = ReaderFactory::create(Type::XLSX);
-        $reader->open($filename);
-        $rows = [];
-        foreach ($reader->getSheetIterator() as $sheet) {
-            foreach ($sheet->getRowIterator() as $row) {
-                $rows[] = $row;
-            }
-        }
-        $reader->close();
+        return (new ExcelSheet)->import($filename);
+        // $reader = ReaderFactory::create(Type::XLSX);
+        // $reader->open($filename);
+        // $rows = [];
+        // foreach ($reader->getSheetIterator() as $sheet) {
+        //     foreach ($sheet->getRowIterator() as $row) {
+        //         $rows[] = $row;
+        //     }
+        // }
+        // $reader->close();
 
-        return $rows;
+        // return $rows;
     }
 
     public function generate($data, $filename = null)
     {
-        if (! $filename) {
-            $filename = tempnam('/tmp', 'ASM');
-        }
-        $writer = WriterFactory::create(Type::XLSX);
-        $writer->openToFile($filename);
-        $writer->addRows($data);
-        $writer->close();
+        return (new ExcelSheet)->generate($data);
+        // if (! $filename) {
+        //     $filename = tempnam('/tmp', 'ASM');
+        // }
+        // $writer = WriterFactory::create(Type::XLSX);
+        // $writer->openToFile($filename);
+        // $writer->addRows($data);
+        // $writer->close();
 
-        return $filename;
+        // return $filename;
     }
 }
