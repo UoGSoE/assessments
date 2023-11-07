8821d9f4d5d2fad8ea97437f4cbe1fe751738d8c
Shift <shift@laravelshift.com>
2023-11-07 13:48:55 +0000

    Adopt anonymous migrations

--- database/migrations/2017_03_10_114441_create_assessments_table.php
+++ database/migrations/2017_03_10_114441_create_assessments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAssessmentsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -34,4 +34,4 @@ class CreateAssessmentsTable extends Migration
     {
         Schema::dropIfExists('assessments');
     }
-}
+};
